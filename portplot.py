bfe9c0a9b94f69919f3c3ba6afa0dba391a43f73
Todd Davis <rtdavis@yahoo.com>
2023-01-21 17:37:16 -0500

    Bug fix for portplot

--- portplot.py
+++ portplot.py
@@ -9,43 +9,42 @@ from matplotlib.widgets import Button as pltButton
 import math
 
 class MainWindow():
-    def __init__(self, root, sym, callback):
+    def __init__(self, root):
         self.root = root
-        self.sym = sym
-        self.callback = callback
         self.dbfile = 'stockscroll.sqlite'
         self.symbols = None
         self.updateNeeded = False
-        self.artistlist = []
-        self.period = 1  # number of days to show, currently up to 7
-        self.mode = 'd'
         self.lockdb = False
+        self.figdict = {}  # Items in dict: fig, ax, artlist 
 
-        self.fig = plt.figure()
-        self.fig.canvas.mpl_connect('close_event', self.on_close)
-        self.ax = self.fig.subplots()
+        self.getQuoteHistory()
+        self.updateHistory()
+        self.updatePlot()
+
+    def addfig(self, sym):
+        if sym in self.figdict.keys():
+            return
+        fig = plt.figure()
+        fig.canvas.mpl_connect('close_event', lambda e, sym=sym: self.on_close(e, sym))
+        ax = fig.subplots()
         plt.subplots_adjust(bottom=0.08)
         plt.plot()
         mngr = plt.get_current_fig_manager()
         mngr.window.wm_geometry('%dx%d+%d+%d' % (1450, 600, 30, 30))
         plt.ion()
-        self.add_buttons()
+        self.add_buttons(sym)
         plt.show()
-
-        self.getQuoteHistory()
-        self.plotPort()
-
-        self.updateHistory()
-        self.updatePlot()
+        self.figdict[sym] = {'fig': fig, 'ax': ax, 'artlist': [], 'mode': 'd', 'per': 1}
+        self.plotPort(sym)
 
     def bringtofront(self):
         print(f'Bring {self.sym} to front')
 
-    def on_close(self, event):
+    def on_close(self, event, sym):
         print(event)
-        self.callback(self.sym)
+        del self.figdict[sym]
 
-    def add_buttons(self):
+    def add_buttons(self, sym):
         x_start = 0.12
         options = [[1,2,5,10], [1,2,5,10]]
         self.day_buttons = []
@@ -60,22 +59,23 @@ class MainWindow():
                 bax = plt.axes(bax_points)
                 bax_points[0] += bax_points[1]*2 + 0.01
                 self.day_buttons.append(pltButton(bax, f'{opt}{mode}'))
-                self.day_buttons[-1].on_clicked(lambda e, per=opt, mode=mode: self.press_button(per, mode))
+                self.day_buttons[-1].on_clicked(lambda e, per=opt, mode=mode, sym=sym: self.press_button(per, mode, sym))
             x_start += (bax_points[1] * 2 + 0.01) * 4 + 0.03
 
-    def press_button(self, per, mode):
-        if self.mode != mode:
-            self.mode = mode
+    def press_button(self, per, mode, sym):
+        if self.figdict[sym]['mode'] != mode:
+            self.figdict[sym]['mode']  = mode
             self.getQuoteHistory()
-        self.period = per
-        self.ax.clear()
-        self.plotPort()
+        self.figdict[sym]['per']  = per
+        self.figdict[sym]['ax'].clear()
+        self.plotPort(sym)
 
     def updatePlot(self):
         delay = 1000*5
         if self.updateNeeded:
-            self.ax.clear()
-            self.plotPort()
+            for sym in self.figdict.keys():
+                self.figdict[sym]['ax'].clear()
+                self.plotPort(sym)
         self.root.after(delay, self.updatePlot)
 
 
@@ -108,67 +108,75 @@ class MainWindow():
         self.root.after(delay, self.updateHistory)
 
 
-    def plotPort(self):
+    def plotPort(self, sym):
+        fig = self.figdict[sym]['fig']
+        ax = self.figdict[sym]['ax']
+        artlist = self.figdict[sym]['artlist']
+        mode = self.figdict[sym]['mode']
+        per = self.figdict[sym]['per']
         data = self.getSymbolPrices()
         self.symbols = [d[0] for d in data if d[0] != 'CASH']
         total = []
         shareDict = {}
         lastprice = {}
         prevclose = {}
-        for sym, price, pclose, shares, _, _ in data:
-            if sym == 'CASH':
+        for s, price, pclose, shares, _, _ in data:
+            if s == 'CASH':
                 cash = util.tryFloat(shares)
             else:
-                shareDict[sym] = shares
-                lastprice[sym] = price
-                prevclose[sym] = pclose
+                shareDict[s] = shares
+                lastprice[s] = price
+                prevclose[s] = pclose
 
-        history, histtimes = self.getdbHistory()
+        history, histtimes = self.getdbHistory(mode)
         if len(histtimes) == 0:
             self.getQuoteHistory()
-            history, histtimes = self.getdbHistory()
+            history, histtimes = self.getdbHistory(mode)
         self.last_time = histtimes[-1]
 
-        if self.period < 10:
-            if self.mode == 'd':
+        if per < 10:
+            if mode == 'd':
                 days_list = list(set([datetime.fromtimestamp(ut).day for ut in histtimes]))
                 dt = datetime.fromtimestamp(self.last_time)
-                day_index = 10 - self.period
+                day_index = 10 - per
                 if day_index < 0: day_index = 0
                 if day_index >= len(days_list): day_index = len(day_index)-1
                 mindate = datetime(dt.year, dt.month, days_list[day_index], 1, 0).timestamp()
-            elif self.mode == 'w':
+            elif mode == 'w':
                 dt = datetime.fromtimestamp(self.last_time)
-                begin_dt = dt - timedelta(days=self.period*7)
+                begin_dt = dt - timedelta(days=per*7)
                 begin_dt = begin_dt.replace(hour=1)
                 mindate = begin_dt.timestamp()
             histtimes = [h for h in histtimes if h > mindate]
-            for sym in history.keys():
-                history[sym] = history[sym][-len(histtimes):]
+            for s in history.keys():
+                history[s] = history[s][-len(histtimes):]
 
-        # ptotal = cash
-        # for sym in self.symbols:
-        #     pp = prevclose.get(sym, 0.0)
-        #     if pp is None: pp = 0.0
-        #     ptotal += pp * shareDict[sym]
-
-        for step in range(len(histtimes)+1):
-            stepTotal = cash
-            for sym in self.symbols:
-                if step >= len(history[sym]):
-                    price = lastprice[sym]
-                else:
-                    price = history[sym][step]
-                if price is None: price = 0.0
-                stepTotal +=  price * shareDict[sym]
-            total.append(stepTotal)
+        if sym == 'TAV':
+            for step in range(len(histtimes)+1):
+                stepTotal = cash
+                for s in self.symbols:
+                    if step >= len(history[s]):
+                        price = lastprice[s]
+                    else:
+                        price = history[s][step]
+                    if price is None: price = 0.0
+                    stepTotal +=  price * shareDict[s]
+                total.append(stepTotal)
+        else:
+            total = history[sym]
 
         ptotal = total[0]
-        self.ax.plot(total, color='black')
-        self.fig.suptitle(f'Total Account Value:{total[-1]:.0f} ({100.0*(total[-1]/ptotal-1):.2f}%)', fontsize=20)
+        ax.plot(total, color='black')
+        if sym == 'TAV':
+            msg = 'Total Account Value: '
+        else:
+            msg = f'{sym}: '
+        msg += f'{total[-1]:.2f} ({100.0*(total[-1]/ptotal-1):.2f}%)'
+        
+        fig.suptitle(msg, fontsize=20)
 
         # Remove previous lines
-        for item in self.artistlist:
+        for item in artlist:
             item.remove()
 
         # Add horizontal lines
@@ -179,22 +187,31 @@ class MainWindow():
             delta_per = 0.5
         while (maxv_per-minv_per)/delta_per > 9:
             delta_per += 0.5
-        self.artistlist = []
-        xlim = self.ax.get_xlim()
-        ylim = self.ax.get_ylim()
-        for percent in np.arange(math.floor(minv_per), math.ceil(maxv_per), delta_per):
+        artlist = []
+        xlim = ax.get_xlim()
+        ylim = ax.get_ylim()
+        if math.floor(minv_per) < 0.0:
+            a = np.arange(0.0, math.floor(minv_per), -delta_per)
+        else:
+            a = []
+        if math.ceil(maxv_per) > 0.0:
+            b = np.arange(delta_per, math.ceil(maxv_per), delta_per)
+        else:
+            b = []
+        perlist = np.concatenate((a, b), axis=0)
+        for percent in perlist:
             yval = ptotal*(1+percent/100.0)
             if yval < ylim[0] or yval > ylim[1]: continue
             if percent < 0.0: color = 'red'
             elif percent > 0.0: color = 'green'
             else: color = 'black'
-            lineitem = self.ax.axhline(y=yval, color=color, linestyle='--')
-            self.artistlist.append(lineitem)
-            atext = self.ax.text(xlim[1], yval, f'  {percent:.1f}%', ha='left', va='center')
-            self.artistlist.append(atext)
+            lineitem = ax.axhline(y=yval, color=color, linestyle='--')
+            artlist.append(lineitem)
+            atext = ax.text(xlim[1], yval, f'  {percent:.1f}%', ha='left', va='center')
+            artlist.append(atext)
 
         # Add vertical lines
-        self.ax.get_xaxis().set_ticks([])
+        ax.get_xaxis().set_ticks([])
         pday = datetime(1972, 1, 1)
         delta = 1
         deltadays = 10
@@ -212,26 +229,27 @@ class MainWindow():
                 if show_vals and look_for_val[i]:
                     if dt.hour >= v:
                         look_for_val[i] = False
-                        lineitem = self.ax.axvline(x=pos, color='y', linestyle=(0, (5, 10)))
-                        self.artistlist.append(lineitem)
+                        lineitem = ax.axvline(x=pos, color='y', linestyle=(0, (5, 10)))
+                        artlist.append(lineitem)
                         if v == 12: hr_text = '12p'
                         elif v == 14: hr_text = '2p'
-                        atext = self.ax.text(pos, ylim[0], hr_text, ha='left', va='bottom')
+                        atext = ax.text(pos, ylim[0], hr_text, ha='left', va='bottom')
             if datetime.fromtimestamp(self.last_time).day == dt.day and show_last_close and dt.hour > 16:
                 show_last_close = False
-                lineitem = self.ax.axvline(x=pos, color='y', linestyle=(0, (5, 10)))
-                self.artistlist.append(lineitem)
-                atext = self.ax.text(pos, ylim[0], '4p', ha='left', va='bottom')
+                lineitem = ax.axvline(x=pos, color='y', linestyle=(0, (5, 10)))
+                artlist.append(lineitem)
+                atext = ax.text(pos, ylim[0], '4p', ha='left', va='bottom')
             if (dt-pday).days >= 1:
                 deltadays += 1
                 pday = dt
             if deltadays >= delta:
                 deltadays = 0
                 look_for_val = [True, True]
-                lineitem = self.ax.axvline(x=pos, color='b', linestyle='--')
-                self.artistlist.append(lineitem)
-                atext = self.ax.text(pos, ylim[0], f' {dt.strftime("%a, %m/%d")}', ha='left', va='bottom')
+                lineitem = ax.axvline(x=pos, color='b', linestyle='--')
+                artlist.append(lineitem)
+                atext = ax.text(pos, ylim[0], f' {dt.strftime("%a, %m/%d")}', ha='left', va='bottom')
 
+        self.figdict[sym]['artlist'] = artlist
         plt.draw()
         plt.pause(0.001)
 
@@ -240,45 +258,45 @@ class MainWindow():
 
         # Convert timestamp to unixsecs: int(timevalues[i].to_pydatetime().timestamp())
         # Convert unixsecs to datetime: datetime.fromtimestamp(unixsecs)
-        
-        if self.mode == 'd':
-            per = '10d'
-            interval = '5m'
-        elif self.mode == 'w':
-            per = '3mo'
-            interval = '60m'
-        history_AAPL = Ticker('AAPL').history(period=per, interval=interval)
-        timevalues = history_AAPL.index.tolist()
-        timevalues = [int(t.to_pydatetime().timestamp()) for sym,t in timevalues]
-        history, histtimes = self.getdbHistory()
-        need_update = False
-        if len(histtimes) == 0: need_update = True
-        if abs(timevalues[0]-histtimes[0]) > 60 * 60: need_update = True
-        if timevalues[-1] - histtimes[-1] > 60*60*2: need_update = True
-        if not need_update:
-            return
-        print(f'len(histtimes)={len(histtimes)}, Start delta (mins): {abs(timevalues[0]-histtimes[0])/(60*60):.1f}, End Delta (hr): {abs(timevalues[-1]-histtimes[-1])/(60*60):.1f}')
+        for mode in ['d', 'w']:
+            if mode == 'd':
+                per = '10d'
+                interval = '5m'
+            elif mode == 'w':
+                per = '3mo'
+                interval = '60m'
+            history_AAPL = Ticker('AAPL').history(period=per, interval=interval)
+            timevalues = history_AAPL.index.tolist()
+            timevalues = [int(t.to_pydatetime().timestamp()) for sym,t in timevalues]
+            history, histtimes = self.getdbHistory(mode)
+            need_update = False
+            if len(histtimes) == 0: need_update = True
+            if abs(timevalues[0]-histtimes[0]) > 60 * 60: need_update = True
+            if timevalues[-1] - histtimes[-1] > 60*60*2: need_update = True
+            if not need_update:
+                return
+            print(f'len(histtimes)={len(histtimes)}, Start delta (mins): {abs(timevalues[0]-histtimes[0])/(60*60):.1f}, End Delta (hr): {abs(timevalues[-1]-histtimes[-1])/(60*60):.1f}')
 
-        self.lockdb = True
-        conn = sqlite3.connect(self.dbfile)
-        cur = conn.cursor()
-        cur.execute(f'DELETE FROM PlotHistory_{self.mode}')
-        for sym, id in iddict.items():
-            history = Ticker(sym).history(period=per, interval=interval)
-            print(sym,end=', ')
-            if history.get('close', None) is None: continue
-            timevalues = history.index.tolist()
-            values = history['close'].to_list()
-            for i, (sym, time) in enumerate(timevalues):
-                unixtime = int(time.to_pydatetime().timestamp())
-                cur.execute(f'INSERT INTO PlotHistory_{self.mode} (stock_id, price, unixtime) VALUES(?,?,?)', (id,values[i],unixtime))
+            self.lockdb = True
+            conn = sqlite3.connect(self.dbfile)
+            cur = conn.cursor()
+            cur.execute(f'DELETE FROM PlotHistory_{mode}')
+            for sym, id in iddict.items():
+                history = Ticker(sym).history(period=per, interval=interval)
+                print(sym,end=', ')
+                if history.get('close', None) is None: continue
+                timevalues = history.index.tolist()
+                values = history['close'].to_list()
+                for i, (sym, time) in enumerate(timevalues):
+                    unixtime = int(time.to_pydatetime().timestamp())
+                    cur.execute(f'INSERT INTO PlotHistory_{mode} (stock_id, price, unixtime) VALUES(?,?,?)', (id,values[i],unixtime))
+                conn.commit()
+
             conn.commit()
-
-        conn.commit()
-        self.lockdb = False
+            self.lockdb = False
         print('Done')
 
-    def getdbHistory(self):
+    def getdbHistory(self, mode):
         '''Returns pricedict{[price]}, [unixtime]'''
         conn = sqlite3.connect(self.dbfile)
         cur = conn.cursor()
@@ -288,7 +306,7 @@ class MainWindow():
             data = self.getSymbolPrices()
             self.symbols = [d[0] for d in data if d[0] != 'CASH']
         for sym in self.symbols:
-            cur.execute(f'SELECT PlotHistory_{self.mode}.price, unixtime, name FROM PlotHistory_{self.mode} JOIN StockList ON PlotHistory_{self.mode}.stock_id = StockList.id WHERE StockList.name=? ORDER BY unixtime', (sym,))
+            cur.execute(f'SELECT PlotHistory_{mode}.price, unixtime, name FROM PlotHistory_{mode} JOIN StockList ON PlotHistory_{mode}.stock_id = StockList.id WHERE StockList.name=? ORDER BY unixtime', (sym,))
             data = cur.fetchall()
             history[sym] = [d[0] for d in data]
             if not havetimes:
@@ -325,11 +343,11 @@ class MainWindow():
         return [(s[0], s[1], s[2], s[3], s[4], s[5]) for s in stocks]
 
 
-    def getLastDate(self):
+    def getLastDate(self, mode):
         '''Returns unixtime for last data point'''
         conn = sqlite3.connect(self.dbfile, uri=True)
         cur = conn.cursor()
-        cur.execute(f'SELECT unixtime FROM PlotHistory_{self.mode} ORDER BY unixtime DESC LIMIT 1')
+        cur.execute(f'SELECT unixtime FROM PlotHistory_{mode} ORDER BY unixtime DESC LIMIT 1')
         data = cur.fetchall()
         if data is None or len(data) == 0: return 0
         return data[0][0]
